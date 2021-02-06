f4d70347ab8ad04386adcf8ebd15a47f35b1a1cb
lyesi <astrophilyes@gmail.com>
2021-02-06 20:58:08 +0100

    crud of item

--- migrations/Version20210205113218.php
+++ migrations/Version20210205113218.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210204171234 extends AbstractMigration
+final class Version20210205113218 extends AbstractMigration
 {
     public function getDescription() : string
     {
